64b71e003c2b70239ff973262c5c43b683989bb2
Jonathan Luk <jluk87@gmail.com>
2019-05-09 04:06:08 +0000

    Automatically backed up by Learn

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,24 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "login", :type => :view do
 
-  before do
-   controller.singleton_class.class_eval do
-     protected
-       def cart
-         session[:cart]
-       end
-       helper_method :cart
-     end
-   end
-
   it "displays 'Login' link if the user is not logged in" do
     visit '/'
     expect(page.body).to have_link('Login', href: root_path)
   end
 
   it "'Hi' message appears if the user is logged in" do
-    session[:name] = 'a session has no name'
-    render root
+    visit '/sessions/new'
+    fill_in 'name', with: 'a session has no name'
+    click_button 'Login'
     expect(page.body).not_to have_link('Login', href: root_path)
   end
 
